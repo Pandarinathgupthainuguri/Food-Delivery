269407c41f2c4cf66ed685ac20e5d6d3e282e608
Pandarinath <nathguptha3639@gmail.com>
2026-05-29 22:19:47 +0530

    explore part 2

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './ExploreMenu.css'
 import {menu_list} from '../../assets/assets/frontend_assets/assets';
 
-function ExploreMenu() {
+function ExploreMenu({category,setCategory}) {
   return (
     <div className='expolre_menu' id='expolre_menu'>
         <h1>Explore our menu</h1>
@@ -11,14 +11,15 @@ function ExploreMenu() {
         {
             menu_list.map((item,idx)=>{
                 return(
-                    <div key={idx} className='explore_menu_list_item'>
-                        <img src={item.menu_image}/>
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={idx} className='explore_menu_list_item'>
+                        <img className={category==item.menu_name?"active":""} src={item.menu_image}/>
                         <p>{item.menu_name}</p>
                     </div>
                 )
             })
         }
        </div>
+       <hr/>
     </div>
   )
 }
